dc32ba8dfc04aedc080f3ad9581309e0b5d9d717
Julian Casaburi <juliancasaburi@gmail.com>
2025-05-19 02:17:00 -0300

    Add 80-remove-duplicates-from-sorted-array-ii

---
+++ 80-remove-duplicates-from-sorted-array-ii/2025-05-19 05.24.08 - Accepted - runtime 77ms - memory 20.5MB.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return len(nums)
+            
+        k = 2
+
+        for i in range(2, len(nums)):
+            if nums[k - 2] != nums[i]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
