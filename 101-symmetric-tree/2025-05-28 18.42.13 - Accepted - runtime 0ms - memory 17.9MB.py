9b69323bee1e5f88433e7f60182170d851bf57a5
Julian Casaburi <juliancasaburi@gmail.com>
2025-05-28 15:45:47 -0300

    Add 101-symmetric-tree

---
+++ 101-symmetric-tree/2025-05-28 18.42.13 - Accepted - runtime 0ms - memory 17.9MB.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def is_mirror(self, t1: Optional[TreeNode], t2: Optional[TreeNode]) -> bool:
+        if not t1 and not t2:
+            return True
+        if not t1 or not t2:
+            return False
+        return (
+            t1.val == t2.val
+            and self.is_mirror(t1.left, t2.right)
+            and self.is_mirror(t1.right, t2.left)
+        )
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return self.is_mirror(root.left, root.right)
