53bbe16f227181bd2ae9b9d255e6f6a8cea39918
Julian Casaburi <juliancasaburi@gmail.com>
2025-09-09 21:09:16 -0300

    Add 1462-list-the-products-ordered-in-a-period

---
+++ 1462-list-the-products-ordered-in-a-period/2025-08-30 00.48.10 - Accepted - runtime 673ms - memory 0.0B.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT product_name, SUM(unit) as unit
+FROM Products INNER JOIN Orders USING(product_id)
+WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY product_name
+HAVING SUM(unit) >= 100
