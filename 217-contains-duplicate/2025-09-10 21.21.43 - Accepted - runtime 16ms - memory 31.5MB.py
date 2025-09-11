6159449dc2a6a64c005e7b5ab6b0c05e3d79794a
Julian Casaburi <juliancasaburi@gmail.com>
2025-09-10 21:27:29 -0300

    Add 217-contains-duplicate

---
+++ 217-contains-duplicate/2025-09-10 21.21.43 - Accepted - runtime 16ms - memory 31.5MB.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        values = set()
+
+        for num in nums:
+            if num in values:
+                return True
+            values.add(num)
+
+        return False
