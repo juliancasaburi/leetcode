88e3a822b3e3f6e03ca2ec6e44a87cee02394309
Julian Casaburi <juliancasaburi@gmail.com>
2025-09-15 00:32:00 -0300

    Add 257-binary-tree-paths

---
+++ 257-binary-tree-paths/2025-09-15 00.27.12 - Accepted - runtime 0ms - memory 17.8MB.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        paths = []
+
+        def dfs(node: Optional[TreeNode], path: str):
+            if not node:
+                return
+            if path:
+                path += "->" + str(node.val)
+            else:
+                path = str(node.val)
+
+            if not node.left and not node.right:
+                paths.append(path)
+            else:
+                dfs(node.left, path)
+                dfs(node.right, path)
+
+        dfs(root, "")
+        return paths
